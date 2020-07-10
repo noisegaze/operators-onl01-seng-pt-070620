80fa1e34d18997e1b5f71608cc35499c79774de1
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-10 19:52:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,6 @@
 def unsafe?(speed)
   speed =95
-  if speed < 40 || > 60
+  if speed < 40 || speed > 60
     unsafe
   else
     safe
@@ -12,7 +12,7 @@ unsafe?
 
 def not_safe?(speed)
   speed = 95
-  speed < 40 || > 60 ? unsafe : safe
+  speed < 40 || speed > 60 ? unsafe : safe
  end
 end
 not_safe?
